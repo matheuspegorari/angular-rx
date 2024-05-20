8166d76e49984d04fb780a7ab66d044e390f47ed
Matheus Pegorari <pegorari42@gmail.com>
2024-05-20 19:55:17 -0300

    refactor: created class

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,8 @@
-import { VolumeInfo, ImageLinks } from './../../models/interfaces';
+import { VolumeInfo, ImageLinks, Item } from './../../models/interfaces';
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Livro } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -19,7 +20,7 @@ export class ListaLivrosComponent implements OnDestroy {
   buscarLivros() {
     this.subscription = this.service.buscar(this.campoBusca).subscribe({
       next: (items) => {
-        this.listaLivros = this.livrosResultadoParaLivros(items)
+        this.listaLivros = this.livrosResultadoParaLivros(items);
       },
       error: (error) => console.error(error),
     });
@@ -35,21 +36,9 @@ export class ListaLivrosComponent implements OnDestroy {
   //   textfile: (data as any).textfile
   // });
 
-  livrosResultadoParaLivros(items):Livro[] {
-    const livros: Livro[] = [];
-
-    items.forEach((item) => {
-      livros.push(this.livro = {
-        title: item.volumeInfo?.title,
-        authors: item.volumeInfo?.authors,
-        publisher: item.volumeInfo?.publisher,
-        publishedDate: item.volumeInfo?.publishedDate,
-        description: item.volumeInfo?.description,
-        previewLink: item.volumeInfo?.previewLink,
-        thumbnail: item.volumeInfo?.imageLinks?.thumbnail,
-      });
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map((item) => {
+      return new LivroVolumeInfo(item);
     });
-
-    return livros
   }
 }
